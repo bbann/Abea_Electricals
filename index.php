d1de63154036a4beea8987e6ad15b83b2541ff16
bbann <91158294+bbann@users.noreply.github.com>
2022-11-09 17:17:11 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page that User will see</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#">ABEA Electricals</a>
+            </div>
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Menu</a></li>
+                <li><a href="">Register</a></li>
+                <li><a href="">Login</a></li>
+                <li><a href="">Logout</a></li>
+                <li><a href="">Brand</a></li>
+                <li><a href="">Category</a></li>
+                <li><a href="">Add Product</a></li>
+                <li><a href="">All Products</a></li>
+                <li><a href="">Cart</a></li>
+                <li>
+                    <form action="/action_page.php">
+                        <input type="text" placeholder="Search.." name="search">
+                        <button type="submit" name="submit" value="submit"><i class="fa fa-search"></i></button>
+                </li>
+                </form>
+            </ul>
+        </div>
+    </nav>
+
+</html>
